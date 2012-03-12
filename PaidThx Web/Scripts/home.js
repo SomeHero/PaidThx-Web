7af1b1049e6205bb594b5cb26a879e034c349bd0
James Rhodes <jrhodes2705@gmail.com>
2012-03-12 01:34:45 -0400

    first commit

---
+++ PaidThx Web/Scripts/home.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="jquery-1.5.1-vsdoc.js" />
+/// <reference path="jquery.validate-vsdoc.js" />
+$(document).ready(function () {
+    $("#btnRequestInvitation").hover(function () {
+        $(this).attr('src', 'Content/images/request-invitation-hover.gif');
+    },
+    function () {
+        $(this).attr('src', 'Content/images/request-invitation.gif');
+    });
+});
