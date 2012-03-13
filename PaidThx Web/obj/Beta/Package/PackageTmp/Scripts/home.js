716bf19b0cdc3b6107383672b90321808158cfbc
James Rhodes <jrhodes2705@gmail.com>
2012-03-13 02:00:29 -0400

    added request invite functionality

--- PaidThx Web/obj/Beta/Package/PackageTmp/Scripts/home.js
+++ PaidThx Web/obj/Beta/Package/PackageTmp/Scripts/home.js
@@ -7,4 +7,62 @@ $(document).ready(function () {
     function () {
         $(this).attr('src', 'Content/images/request-invitation.gif');
     });
+    $("#btnRequestInvitation").click(function () {
+        $("#beta-signup-form").validate({
+            rules: {
+                EmailAddress: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                EmailAddress: {
+                    required: 'Enter your email address to receive your invite.',
+                    email: 'Your email address must be in the format of name@domain.com'
+                }
+            },
+            submitHandler: function (form) {
+                submitSignup();
+            },
+            errorPlacement: function (error, element) {
+                error.appendTo($('#output'));
+                $('#output').show();
+            }
+        });
+    });
 });
+var getBaseURL = function() {
+    return location.protocol + "//" + location.hostname +
+      (location.port && ":" + location.port) + "/";
+}
+var submitSignup = function () {
+    var serviceUrl = getBaseURL() + 'services/BetaSignUpService/BetaSignUps?apiKey=BDA11D91-7ADE-4DA1-855D-24ADFE39D174';
+    var emailAddress = $("#txtEmailAddress").val();
+
+    var signUpData = {
+        "emailAddress": emailAddress
+    };
+    var jsonData = $.toJSON(signUpData);
+
+    $.ajax({
+        type: "POST",
+        url: serviceUrl,
+        data: jsonData,
+        contentType: "application/json",
+        dataType: "json",
+        processData: false,
+        success: function (data) {
+            if (data.success) {
+                $('#output').html(data.message);
+                $('#txtEmailAddress').val('');
+            }
+            else {
+                $('#output').html(data.message);
+            }
+            $('#output').show();
+        },
+        error: function (objRequest, next, errorThrown) {
+            alert(objRequest.responseText);
+        }
+    });
+}
